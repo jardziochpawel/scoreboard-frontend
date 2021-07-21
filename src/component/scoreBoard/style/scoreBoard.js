53babafa97d40b751ccfef8cd2e19995849d93cd
silus91 <wizard91@o2.pl>
2021-07-21 09:44:57 +0100

    added hmbgb image changed stiling for it

--- src/component/scoreBoard/style/scoreBoard.js
+++ src/component/scoreBoard/style/scoreBoard.js
@@ -168,7 +168,7 @@ export const RKP = styled.div`
   img{
     width: 90px;
   }
-`;
+  background-image: radial-gradient(white 30%, rgba(255, 255, 255, 0.01) 70%);`;
 
 export const BLShield = styled.div`
   position: absolute;
